3ad013861496fca1c9236e2e41f6958ac1f78ec0
BinhTran <fam.out16@gmail.com>
2024-08-07 20:30:22 +0700

    Add cac files cua bai thuc hanh giai phuong trinh co dang ax + b = 0

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace CAULENHDIEUKIEN
+{
+    class LinearEquationResolver
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float b;
+            float c;
+            float x;
+
+            Console.WriteLine("Giai phuong trinh co dang ax + b = 0");
+            Console.WriteLine("Nhap he so a:");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap he so b:");
+            b = float.Parse(Console.ReadLine());
+
+            if (a!=0)
+            {
+                x = - b / a;
+                Console.WriteLine("Phuong trinh co nghiem: " + x);
+            }
+            else if (b!=0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh co vo so nghiem");
+            }
+        }
+    }
+}
